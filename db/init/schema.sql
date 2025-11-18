c5dcdd7aec2f8bfc3d3ad043fbcc3421fcbef3b0
billy.rivera <briveras001@uaemex.mx>
2025-11-17 19:01:03 -0600

    first commit

---
+++ db/init/schema.sql
@@ -0,0 +1,51 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS cat_role (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(70) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS users (
+	id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+	name VARCHAR(100) NOT NULL,
+	lastname1 VARCHAR(100) NOT NULL,
+	lastname2 VARCHAR(100),
+	genre CHAR(1) CHECK(genre IN ('M', 'F')) NOT NULL,
+	role INTEGER NOT NULL,
+	created_at TIMESTAMPTZ DEFAULT NOW(),	
+	updated_at TIMESTAMPTZ,
+	deleted_at TIMESTAMPTZ
+);
+
+CREATE TABLE IF NOT EXISTS account (
+	id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+	id_user UUID NOT NULL UNIQUE,
+	email VARCHAR(100) NOT NULL UNIQUE,
+	password VARCHAR(255) NOT NULL,
+	created_at TIMESTAMPTZ DEFAULT NOW(),	
+	updated_at TIMESTAMPTZ,
+	deleted_at TIMESTAMPTZ 
+);
+
+CREATE TABLE IF NOT EXISTS client_membership (
+	id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+	id_user UUID NOT NULL UNIQUE,
+	membership_type INTEGER,
+	start_date DATE NOT NULL, 
+	end_date DATE NOT NULL,
+	created_at TIMESTAMPTZ DEFAULT NOW(),	
+	updated_at TIMESTAMPTZ,
+	deleted_at TIMESTAMPTZ
+);
+
+ALTER TABLE users
+ADD CONSTRAINT fk_users_role
+FOREIGN KEY (role) REFERENCES cat_role(id);
+
+ALTER TABLE account
+ADD CONSTRAINT fk_account_user
+FOREIGN KEY (id_user) REFERENCES users(id);
+
+ALTER TABLE client_membership
+ADD CONSTRAINT fk_membership_user
+FOREIGN KEY (id_user) REFERENCES users(id);
---
+++ gym/src/main/java/com/ax01/gym/GymApplication.java
@@ -0,0 +1,13 @@
+package com.ax01.gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymApplication.class, args);
+	}
+
+}
